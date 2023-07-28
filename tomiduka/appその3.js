42ddb01234ea4b65c70a19bd284b8342b9e833c3
双葉 <104970783+Futaba-rgb@users.noreply.github.com>
2023-07-28 23:20:18 +0900

    Add files via upload

---
+++ tomiduka/appその3.js
@@ -0,0 +1,321 @@
+var paiYama = [
+  { "No": 0, "paiName": "一萬", "paiCode": "man1", "paiNo": 1 },
+  { "No": 1, "paiName": "一萬", "paiCode": "man1", "paiNo": 1 },
+  { "No": 2, "paiName": "一萬", "paiCode": "man1", "paiNo": 1 },
+  { "No": 3, "paiName": "一萬", "paiCode": "man1", "paiNo": 1 },
+  { "No": 6, "paiName": "二萬", "paiCode": "man2", "paiNo": 2 },
+  { "No": 7, "paiName": "二萬", "paiCode": "man2", "paiNo": 2 },
+  { "No": 10, "paiName": "三萬", "paiCode": "man3", "paiNo": 3 },
+  { "No": 11, "paiName": "三萬", "paiCode": "man3", "paiNo": 3 },
+  { "No": 14, "paiName": "四萬", "paiCode": "man4", "paiNo": 4 },
+  { "No": 15, "paiName": "四萬", "paiCode": "man4", "paiNo": 4 },
+  { "No": 18, "paiName": "五萬", "paiCode": "man5", "paiNo": 5 },
+  { "No": 19, "paiName": "五萬", "paiCode": "man5", "paiNo": 5 },
+  { "No": 22, "paiName": "六萬", "paiCode": "man6", "paiNo": 6 },
+  { "No": 23, "paiName": "六萬", "paiCode": "man6", "paiNo": 6 },
+  { "No": 24, "paiName": "七萬", "paiCode": "man7", "paiNo": 7 },
+  { "No": 27, "paiName": "七萬", "paiCode": "man7", "paiNo": 7 },
+  { "No": 30, "paiName": "八萬", "paiCode": "man8", "paiNo": 8 },
+  { "No": 31, "paiName": "八萬", "paiCode": "man8", "paiNo": 8 },
+  { "No": 32, "paiName": "九萬", "paiCode": "man9", "paiNo": 9 },
+  { "No": 33, "paiName": "九萬", "paiCode": "man9", "paiNo": 9 },
+  { "No": 34, "paiName": "九萬", "paiCode": "man9", "paiNo": 9 },
+  { "No": 35, "paiName": "九萬", "paiCode": "man9", "paiNo": 9 },
+
+  { "No": 36, "paiName": "一筒", "paiCode": "pin1", "paiNo": 11 },
+  { "No": 37, "paiName": "一筒", "paiCode": "pin1", "paiNo": 11 },
+  { "No": 38, "paiName": "一筒", "paiCode": "pin1", "paiNo": 11 },
+  { "No": 39, "paiName": "一筒", "paiCode": "pin1", "paiNo": 11 },
+  { "No": 42, "paiName": "二筒", "paiCode": "pin2", "paiNo": 12 },
+  { "No": 43, "paiName": "二筒", "paiCode": "pin2", "paiNo": 12 },
+  { "No": 44, "paiName": "三筒", "paiCode": "pin3", "paiNo": 13 },
+  { "No": 47, "paiName": "三筒", "paiCode": "pin3", "paiNo": 13 },
+  { "No": 48, "paiName": "四筒", "paiCode": "pin4", "paiNo": 14 },
+  { "No": 51, "paiName": "四筒", "paiCode": "pin4", "paiNo": 14 },
+  { "No": 52, "paiName": "五筒", "paiCode": "pin5", "paiNo": 15 },
+  { "No": 55, "paiName": "五筒", "paiCode": "pin5", "paiNo": 15 },
+  { "No": 58, "paiName": "六筒", "paiCode": "pin6", "paiNo": 16 },
+  { "No": 59, "paiName": "六筒", "paiCode": "pin6", "paiNo": 16 },
+  { "No": 62, "paiName": "七筒", "paiCode": "pin7", "paiNo": 17 },
+  { "No": 63, "paiName": "七筒", "paiCode": "pin7", "paiNo": 17 },
+  { "No": 66, "paiName": "八筒", "paiCode": "pin8", "paiNo": 18 },
+  { "No": 67, "paiName": "八筒", "paiCode": "pin8", "paiNo": 18 },
+  { "No": 68, "paiName": "九筒", "paiCode": "pin9", "paiNo": 19 },
+  { "No": 69, "paiName": "九筒", "paiCode": "pin9", "paiNo": 19 },
+  { "No": 70, "paiName": "九筒", "paiCode": "pin9", "paiNo": 19 },
+  { "No": 71, "paiName": "九筒", "paiCode": "pin9", "paiNo": 19 },
+
+  { "No": 72, "paiName": "一索", "paiCode": "sou1", "paiNo": 21 },
+  { "No": 73, "paiName": "一索", "paiCode": "sou1", "paiNo": 21 },
+  { "No": 74, "paiName": "一索", "paiCode": "sou1", "paiNo": 21 },
+  { "No": 75, "paiName": "一索", "paiCode": "sou1", "paiNo": 21 },
+  { "No": 78, "paiName": "二索", "paiCode": "sou2", "paiNo": 22 },
+  { "No": 79, "paiName": "二索", "paiCode": "sou2", "paiNo": 22 },
+  { "No": 80, "paiName": "三索", "paiCode": "sou3", "paiNo": 23 },
+  { "No": 83, "paiName": "三索", "paiCode": "sou3", "paiNo": 23 },
+  { "No": 84, "paiName": "四索", "paiCode": "sou4", "paiNo": 24 },
+  { "No": 87, "paiName": "四索", "paiCode": "sou4", "paiNo": 24 },
+  { "No": 88, "paiName": "五索", "paiCode": "sou5", "paiNo": 25 },
+  { "No": 91, "paiName": "五索", "paiCode": "sou5", "paiNo": 25 },
+  { "No": 92, "paiName": "六索", "paiCode": "sou6", "paiNo": 26 },
+  { "No": 95, "paiName": "六索", "paiCode": "sou6", "paiNo": 26 },
+  { "No": 96, "paiName": "七索", "paiCode": "sou7", "paiNo": 27 },
+  { "No": 99, "paiName": "七索", "paiCode": "sou7", "paiNo": 27 },
+  { "No": 100, "paiName": "八索", "paiCode": "sou8", "paiNo": 28 },
+  { "No": 101, "paiName": "八索", "paiCode": "sou8", "paiNo": 28 },
+  { "No": 102, "paiName": "八索", "paiCode": "sou8", "paiNo": 28 },
+  { "No": 103, "paiName": "八索", "paiCode": "sou8", "paiNo": 28 },
+  { "No": 104, "paiName": "九索", "paiCode": "sou9", "paiNo": 29 },
+  { "No": 105, "paiName": "九索", "paiCode": "sou9", "paiNo": 29 },
+  { "No": 106, "paiName": "九索", "paiCode": "sou9", "paiNo": 29 },
+  { "No": 107, "paiName": "九索", "paiCode": "sou9", "paiNo": 29 },
+
+  { "No": 108, "paiName": "東", "paiCode": "ji1", "paiNo": 31 },
+  { "No": 109, "paiName": "東", "paiCode": "ji1", "paiNo": 31 },
+  { "No": 110, "paiName": "東", "paiCode": "ji1", "paiNo": 31 },
+  { "No": 111, "paiName": "東", "paiCode": "ji1", "paiNo": 31 },
+  { "No": 112, "paiName": "南", "paiCode": "ji2", "paiNo": 32 },
+  { "No": 113, "paiName": "南", "paiCode": "ji2", "paiNo": 32 },
+  { "No": 114, "paiName": "南", "paiCode": "ji2", "paiNo": 32 },
+  { "No": 115, "paiName": "南", "paiCode": "ji2", "paiNo": 32 },
+  { "No": 116, "paiName": "西", "paiCode": "ji3", "paiNo": 33 },
+  { "No": 117, "paiName": "西", "paiCode": "ji3", "paiNo": 33 },
+  { "No": 118, "paiName": "西", "paiCode": "ji3", "paiNo": 33 },
+  { "No": 119, "paiName": "西", "paiCode": "ji3", "paiNo": 33 },
+  { "No": 120, "paiName": "北", "paiCode": "ji4", "paiNo": 34 },
+  { "No": 121, "paiName": "北", "paiCode": "ji4", "paiNo": 34 },
+  { "No": 122, "paiName": "北", "paiCode": "ji4", "paiNo": 34 },
+  { "No": 123, "paiName": "北", "paiCode": "ji4", "paiNo": 34 },
+  { "No": 124, "paiName": "白", "paiCode": "ji5", "paiNo": 35 },
+  { "No": 125, "paiName": "白", "paiCode": "ji5", "paiNo": 35 },
+  { "No": 126, "paiName": "白", "paiCode": "ji5", "paiNo": 35 },
+  { "No": 127, "paiName": "白", "paiCode": "ji5", "paiNo": 35 },
+  { "No": 128, "paiName": "發", "paiCode": "ji6", "paiNo": 36 },
+  { "No": 129, "paiName": "發", "paiCode": "ji6", "paiNo": 36 },
+  { "No": 130, "paiName": "發", "paiCode": "ji6", "paiNo": 36 },
+  { "No": 131, "paiName": "發", "paiCode": "ji6", "paiNo": 36 },
+  { "No": 132, "paiName": "中", "paiCode": "ji7", "paiNo": 37 },
+  { "No": 133, "paiName": "中", "paiCode": "ji7", "paiNo": 37 },
+  { "No": 134, "paiName": "中", "paiCode": "ji7", "paiNo": 37 },
+  { "No": 135, "paiName": "中", "paiCode": "ji7", "paiNo": 37 }
+];
+
+var container = document.getElementById("paiContainer");
+var usedPai = [];    // 既に表示された牌を保存する配列
+var storedPai = [];  // 画面の中央に表示されている牌を保存する配列
+var displayedPaiContainer = document.createElement("div"); // Create a new container for displayedPai tiles
+displayedPaiContainer.classList.add("displayed-pai-container"); // Add a class to the new container
+document.body.appendChild(displayedPaiContainer); // Append the container to the body
+
+// Function to display the storedPai tiles on the screen after sorting
+function displayStoredPai() {
+  // Sort the storedPai array by the paiNo property
+  storedPai.sort(function (a, b) {
+    return a.paiNo - b.paiNo;
+  });
+
+  // Clear the container before displaying the sorted tiles
+  container.innerHTML = "";
+
+  // Display the sorted tiles on the screen
+  storedPai.forEach(function (pai) {
+    var newPai = document.createElement("div");
+    newPai.classList.add(pai.paiCode);
+    newPai.classList.add("displayedPai"); // Add the displayedPai class
+
+    container.appendChild(newPai);
+
+    // 牌をクリックしたときの処理を追加
+    newPai.addEventListener("click", function () {
+      // 牌を置き換える処理を実行
+      displayStoredPai(); // Display the storedPai tiles after sorting
+      moveToStoredPai();
+
+      // 国士無双の役の判定を行う
+      const isWinningHand = isKokushiMusou();
+      if (isWinningHand) {
+        // 国士無双の役が成立した場合、appその4.htmlに遷移する
+        window.location.href = "appその4.html";
+      }
+    });
+  });
+  // Display the storedPai tiles at the bottom of the screen
+  displayedPaiContainer.innerHTML = "";
+  storedPai.forEach(function (pai) {
+    var newPai = document.createElement("div");
+    newPai.classList.add(pai.paiCode);
+    newPai.classList.add("displayedPai"); // Add the displayedPai class
+
+    displayedPaiContainer.appendChild(newPai);
+  });
+}
+
+// 牌を表示する関数
+function showPai(paiData) {
+  // 既存の牌を削除
+  var existingPai = container.firstChild;
+  if (existingPai) {
+    container.removeChild(existingPai);
+  }
+
+  // 新しい牌を生成して表示
+  var newPai = document.createElement("div");
+  newPai.classList.add(paiData.paiCode);
+  container.appendChild(newPai);
+
+  // 牌をクリックしたときの処理
+  newPai.addEventListener("click", function () {
+    // 牌を置き換える処理を実行
+    displayStoredPai(); // Display the storedPai tiles after sorting
+    moveToStoredPai();
+
+    // 国士無双の役の判定を行う
+    const isWinningHand = isKokushiMusou();
+    if (isWinningHand) {
+      // 国士無双の役が成立した場合、appその4.htmlに遷移する
+      window.location.href = "appその4.html";
+    }
+  });
+}
+
+// 一つの牌を表示する関数（ランダムに選択）
+function showRandomPai() {
+  if (paiYama.length === 0) {
+    alert("残りの牌はありません。");
+    return;
+  }
+
+  var randomIndex = Math.floor(Math.random() * paiYama.length);
+  var selectedPai = paiYama[randomIndex];
+
+  // Clear the existing displayed pai from paiContainer
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+
+  // 新しい牌を表示
+  showPai(selectedPai);
+
+  // 選ばれた牌をpaiYama配列から削除
+  paiYama.splice(randomIndex, 1);
+
+  // 選ばれた牌をusedPai配列に追加
+  usedPai.push(selectedPai); // Add the selected tile to the usedPai array
+
+  // Clear the displayedPaiContainer before displaying the new random tile
+  displayedPaiContainer.innerHTML = "";
+  storedPai.forEach(function (pai) {
+    var newPai = document.createElement("div");
+    newPai.classList.add(pai.paiCode);
+    newPai.classList.add("displayedPai"); // Add the displayedPai class
+
+    displayedPaiContainer.appendChild(newPai);
+  });
+}
+
+// 新たに表示する牌をpaiYama配列からランダムに選択し、画面の中央の牌と置き換える関数
+function replaceRandomPai() {
+  if (paiYama.length === 0) {
+    alert("残りの牌はありません。");
+    return;
+  }
+
+  var randomIndex = Math.floor(Math.random() * paiYama.length);
+  var selectedPai = paiYama[randomIndex];
+
+  // 新しい牌を表示
+  showPai(selectedPai);
+
+  // 選ばれた牌をpaiYama配列から削除
+  paiYama.splice(randomIndex, 1);
+
+  // 置き換える前の画面に表示されていた牌をstoredPaiに移動
+  if (usedPai.length > 0) {
+    var lastPai = usedPai.pop();
+    storedPai.push(lastPai);
+
+    // storedPaiを牌番号で並び替え
+    storedPai.sort(function (a, b) {
+      return a.paiNo - b.paiNo;
+    })
+  }
+}
+
+// 画面に表示されている牌をusedPaiからstoredPaiに移動する関数
+function moveToStoredPai() {
+  if (usedPai.length === 0) {
+    alert("まだ表示されている牌がありません。");
+    return;
+  }
+
+  var lastPai = usedPai.pop();
+  storedPai.push(lastPai);
+
+  // 画面に表示されている牌を更新
+  if (usedPai.length === 0) {
+    // 表示されている牌がなくなった場合、画面を空にする
+    container.innerHTML = "";
+  } else {
+    // 最後に表示された牌を表示
+    showPai(usedPai[usedPai.length - 1]);
+  }
+
+  // Remove the event listener from the displayedPai tiles
+  displayedPaiContainer.querySelectorAll(".displayedPai").forEach(function (tile) {
+    tile.removeEventListener("click", replaceRandomPai);
+  });
+
+  // 新しい牌を表示 (replace the clicked tile with a new random tile from paiYama)
+  showRandomPai();
+}
+
+function isKokushiMusou() {
+  // Count unique tiles in the storedPai array
+  const uniqueTiles = new Set(storedPai.map(pai => pai.paiName));
+
+  // Check if all 13 unique tiles required for Kokushi Musou are present
+  const requiredTiles = new Set([
+    "一萬", "九萬", "一筒", "九筒", "一索", "九索", "東", "南", "西", "北", "白", "發", "中"
+  ]||[
+    "一萬", "九萬", "一筒", "九筒", "一索", "九索", "東", "南", "西", "北", "白", "發", "中","一萬",
+  ]||[
+    "一萬", "九萬", "一筒", "九筒", "一索", "九索", "東", "南", "西", "北", "白", "發", "中","九萬",
+  ]||[
+    "一萬", "九萬", "一筒", "九筒", "一索", "九索", "東", "南", "西", "北", "白", "發", "中","一筒",
+  ]||[
+    "一萬", "九萬", "一筒", "九筒", "一索", "九索", "東", "南", "西", "北", "白", "發", "中","九筒",
+  ]||[
+    "一萬", "九萬", "一筒", "九筒", "一索", "九索", "東", "南", "西", "北", "白", "發", "中","一索",
+  ]||[
+    "一萬", "九萬", "一筒", "九筒", "一索", "九索", "東", "南", "西", "北", "白", "發", "中","九索",
+  ]||[
+    "一萬", "九萬", "一筒", "九筒", "一索", "九索", "東", "南", "西", "北", "白", "發", "中","東",
+  ]||[
+    "一萬", "九萬", "一筒", "九筒", "一索", "九索", "東", "南", "西", "北", "白", "發", "中","南",
+  ]||[
+    "一萬", "九萬", "一筒", "九筒", "一索", "九索", "東", "南", "西", "北", "白", "發", "中","西",
+  ]||[
+    "一萬", "九萬", "一筒", "九筒", "一索", "九索", "東", "南", "西", "北", "白", "發", "中","北",
+  ]||[
+    "一萬", "九萬", "一筒", "九筒", "一索", "九索", "東", "南", "西", "北", "白", "發", "中","白",
+  ]||[
+    "一萬", "九萬", "一筒", "九筒", "一索", "九索", "東", "南", "西", "北", "白", "發", "中","發",
+  ]||[
+    "一萬", "九萬", "一筒", "九筒", "一索", "九索", "東", "南", "西", "北", "白", "發", "中","中",
+  ]);
+
+  // Add the condition for the extra tile (14th tile) of any kind
+  return uniqueTiles.size === 13 && [...uniqueTiles].every(tile => requiredTiles.has(tile));
+}
+
+// DOMの読み込みが完了した後に実行する処理
+window.addEventListener("DOMContentLoaded", function () {
+
+  // Show the initial random pai when the page is loaded
+  showRandomPai();
+
+  // ボタンクリック時の処理
+  document.getElementById("showButton").addEventListener("click", function () {
+    showRandomPai(); // Call the showRandomPai function when the button is clicked
+  });
+});
